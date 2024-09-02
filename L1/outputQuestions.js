3c25b0fb4b555440325e7b29f5b25c4e1567db59
_Saurav_ <sauravsharma266@gmail.com>
2024-09-02 19:49:25 +0530

    first lecture

---
+++ L1/outputQuestions.js
@@ -0,0 +1,41 @@
+//SCOPE
+
+
+//START
+// {
+//     let a=5; // change let to var
+    
+// }
+
+// console.log(a);
+
+// END
+
+
+//Variable shadowing
+
+//try with combinations of var and let
+// function test(){
+//     let a="hello";
+//     if(true){
+//         let a = "hi";
+//         console.log(a);
+//     }
+//     console.log(a);
+    
+// }
+
+// test();
+
+//Temporal Dead Zone
+ 
+// function abc(){
+//     console.log(b);
+//     // let a = 5; 
+//     var b = 5;
+// }
+
+
+// abc();
+
+
